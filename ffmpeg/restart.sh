98f277956b4c4c3ddbc7516b5e6848d05316b25d
Alex Nichol <alexnichol@comcast.net>
2012-06-15 12:42:28 -0400

    ffmpeg compiles with OGG support

--- ffmpeg/restart.sh
+++ ffmpeg/restart.sh
@@ -1,5 +1,6 @@
+# usage: restart.sh x264 libvorbis_path libogg_path libtheora_path
 if [ -f config.mak ]; then
 	rm config.mak
 fi
-./configure --disable-yasm --enable-gpl --enable-nonfree --enable-libx264 --enable-avfilter "--extra-ldflags=-L$1" "--extra-cflags=-I$1"
+./configure --disable-yasm --enable-gpl --enable-nonfree --enable-libx264 --enable-avfilter --enable-libvorbis --enable-libtheora "--extra-ldflags=-L$1" "--extra-cflags=-I$1" "--extra-ldflags=-L$2/libs" "--extra-cflags=-I$2/include" "--extra-ldflags=-L$3" "--extra-cflags=-I$3" "--extra-ldflags=-L$4" "--extra-cflags=-I$4"
 make clean
